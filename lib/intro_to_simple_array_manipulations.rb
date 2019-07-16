c8c025a5108727bbeedba3ac414fa9f7a81d0d9e
Catherine Yang <liahnee@gmail.com>
2019-07-16 15:25:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,6 +22,22 @@ def using_concat(array, array2)
   array.concat(array2)
 end 
 
-def using_insert
-  array.insert()
+def using_insert(index, obj)
+  array.insert(index, obj)
+end 
+
+def using_uniq
+  
+end
+
+def using_flatten
+  
+end 
+
+def using_delete
+  
+end
+
+def using_delete_at
+  
 end 
