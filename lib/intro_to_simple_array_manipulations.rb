84cd85dcbf077b803d38f58810454def261989d7
Catherine Yang <liahnee@gmail.com>
2019-07-16 15:21:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,7 +6,16 @@ def using_pop(array)
   array.pop
 end 
 
-def pop_with_args(array, )
-  array.pop
-  array.pop
-  return array 
+def pop_with_args(array, n)
+  array.pop(n)
+end
+
+def using_shift(array)
+  array.shift 
+end 
+
+def shift_with_args(array, n)
+ array.shift(n)
+end
+ 
+ 
